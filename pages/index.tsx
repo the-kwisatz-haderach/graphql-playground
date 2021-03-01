a88c41237d6b8031898bfbd1ee7c6da0b9a18e94
Gustaf Lundström <gustaflundstrom@Gustafs-MacBook-Pro.local>
2021-03-01 14:38:52 +0100

    Update styles and index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,17 @@ import { useQuery } from '@apollo/client'
 import { useEffect, useRef, useState } from 'react'
 import { GetStaticProps } from 'next'
 import mapboxgl from 'mapbox-gl'
+import Main from '../layouts/Main'
 import { ILocation } from '../types/graphql'
 import connectToDb from '../db/utils/connectToDb'
+import convertToGeoJSON from '../lib/mapBoxUtils/convertToGeoJSON'
+import Locator from '../components/Locator'
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOXGL_ACCESS_TOKEN as string
 
 type Props = {
   locations: ILocation[]
 }
-
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOXGL_ACCESS_TOKEN as string
 
 function HomePage({ locations }: Props) {
   const mapContainer = useRef<HTMLDivElement>(null)
@@ -26,17 +29,13 @@ function HomePage({ locations }: Props) {
         container: mapContainer.current,
         style: 'mapbox://styles/mapbox/light-v10',
         center: [lng, lat],
-        zoom: zoom,
+        zoom,
       })
 
-      // locations.forEach((location) => {
-      //   new mapboxgl.Marker()
-      //     .setLngLat([
-      //       +location.coordinates.longitude,
-      //       +location.coordinates.latitude,
-      //     ])
-      //     .addTo(map)
-      // })
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false,
+      })
 
       map.on('move', () => {
         setLng(+map.getCenter().lng.toFixed(4))
@@ -45,43 +44,13 @@ function HomePage({ locations }: Props) {
       })
 
       map.on('load', () => {
-        map.addSource('points', {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: [
-              {
-                type: 'Feature',
-                properties: {},
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-91.395263671875, -0.9145729757782163],
-                },
-              },
-              {
-                type: 'Feature',
-                properties: {},
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-90.32958984375, -0.6344474832838974],
-                },
-              },
-              {
-                type: 'Feature',
-                properties: {},
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-91.34033203125, 0.01647949196029245],
-                },
-              },
-            ],
-          },
-        })
+        const layerName = 'points'
 
+        map.addSource(layerName, convertToGeoJSON(locations))
         map.addLayer({
           id: 'symbols',
           type: 'circle',
-          source: 'points',
+          source: layerName,
           paint: {
             'circle-color': '#918af7',
             'circle-stroke-color': '#3327e5',
@@ -99,35 +68,61 @@ function HomePage({ locations }: Props) {
       })
 
       // Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
-      map.on('mouseenter', 'symbols', function () {
+      map.on('mouseenter', 'symbols', function (e) {
         map.getCanvas().style.cursor = 'pointer'
+        const coordinates = e.features[0].geometry.coordinates.slice()
+        const description = e.features[0].properties.description
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
+        }
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(coordinates).setHTML(description).addTo(map)
       })
 
       // Change it back to a pointer when it leaves.
       map.on('mouseleave', 'symbols', function () {
         map.getCanvas().style.cursor = ''
+        popup.remove()
       })
+
+      // Add zoom and rotation controls to the map.
+      map.addControl(new mapboxgl.NavigationControl())
     }
     return () => map.remove()
   }, [mapContainer, locations])
 
   return (
-    <div>
+    <Main>
       <Head>
         <link
-          href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css"
+          href={process.env.NEXT_PUBLIC_MAPBOXGL_CSS_HREF}
           rel="stylesheet"
         />
       </Head>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      <div className="locator-container">
+        <Locator lat={lat} lng={lng} zoom={zoom} />
       </div>
       <div
         id="map"
         ref={mapContainer}
         style={{ height: '100vh', width: '100vw' }}
       />
-    </div>
+      <style jsx>{`
+        .locator-container {
+          width: 400px;
+          position: absolute;
+          left: calc(50% - 200px);
+          top: 10px;
+          z-index: 1;
+        }
+      `}</style>
+    </Main>
   )
 }
 
